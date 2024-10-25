75e9a2ca5b988429e8b02e372053b693e964febc
vystii <ngahabanda@gmail.com>
2024-10-25 08:32:57 +0200

    add study field and alter study level as fk

--- VayBe/v_utilities/models.py
+++ VayBe/v_utilities/models.py
@@ -1,21 +1,36 @@
+from email.utils import unquote
+from wsgiref.validate import validator
 from django.db import models
+from django.core import validators
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-class ModelBase(models.Model):
-    STUDIES_LEVELS = [
-        ('L1', 'Licence 1'),
-        ('L2', 'Licence 2'),
-        ('L3', 'Licence 3'),
-        ('M1', 'Master 1'),
-        ('M2', 'Master 2')
-    ]
-
-    studies_level = models.CharField(
-        max_length=2,
-        choices=STUDIES_LEVELS,
-        default='L1',
-    )
+class StudyLevel(models.Model):
+    level_validator = validators.MinValueValidator(1)
+    level = models.IntegerField(_("level"), unique=True, validators=[level_validator])
+    code = models.CharField(_("code level"), max_length=5, primary_key=True)
+    label = models.CharField(_("label"), max_length=20,blank=False)
+    REQUIRED = ["niveau", "label"]
     
+    constraints = [
+            models.UniqueConstraint(fields=['level', 'code'], name='level_code_id'),
+        ]
+    
+    def __str__(self):
+        return self.label
+    
+class StudyField(models.Model):
+    code_validator = validators.RegexValidator("^[A-Z]{3}")
+    code = models.CharField(_("code"), max_length=3, primary_key=True, validators=[code_validator])
+    label = models.CharField(_("Filiaire"), max_length=20)
+    def __str__(self):
+        return f"{self.code}-{self.label}"
+
+class ModelBase(models.Model):
+
+    studies_level = models.ForeignKey(StudyLevel, verbose_name=_("studies level"), on_delete=models.CASCADE, blank=False)
+    study_field = models.ForeignKey(StudyField, verbose_name=_("study field"), on_delete=models.CASCADE, blank = False)
     class Meta:
         abstract = True
+        
