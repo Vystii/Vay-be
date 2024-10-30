3edacabbac49bd3363e4db04cc28e3968fd8d92f
vystii <ngahabanda@gmail.com>
2024-10-30 04:38:13 +0100

    add entity to admin

--- VayBe/v_utilities/admin.py
+++ VayBe/v_utilities/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from v_utilities.models import StudyField, StudyLevel
+
 # Register your models here.
+@admin.register(StudyLevel)
+class StudyLevelAdmn(admin.ModelAdmin):
+    pass
+
+@admin.register(StudyField)
+class StudyFieldAdmn(admin.ModelAdmin):
+    pass
