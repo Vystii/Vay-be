f7e845022e48a67a8ce3dd22b3d58595cecb3d73
vystii <ngahabanda@gmail.com>
2025-01-03 23:17:23 +0100

    maj for container

--- VayBe/v_utilities/services.py
+++ VayBe/v_utilities/services.py
@@ -1,12 +1,15 @@
 from django.utils import timezone
 from v_utilities.models import SiteConfiguration
-
+from django.db.utils import ProgrammingError, OperationalError
 
 class VUtilitiesService:
     @staticmethod
-    def getCurrentYear()->int:
+    def getCurrentYear() -> int:
         year = None
-        if SiteConfiguration.objects.filter(pk = "current_year").count() : 
-            _year: str = SiteConfiguration.objects.get(pk = "current_year").config_value
-            year = int(_year.split("/")[0])
-        return  year if year else  int(timezone.now().year)
+        try:
+            if SiteConfiguration.objects.filter(pk="current_year").exists():
+                _year: str = SiteConfiguration.objects.get(pk="current_year").config_value
+                year = int(_year.split("/")[0])
+        except (ProgrammingError, OperationalError):
+            pass
+        return year if year else int(timezone.now().year)
