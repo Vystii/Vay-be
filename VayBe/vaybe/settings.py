1b1ca64635a27fac8ac3d29d3d891ccf37bee225
vystii <ngahabanda@gmail.com>
2025-01-04 07:27:58 +0100

    replacing settings.example with .env-example

--- VayBe/vaybe/settings.py
+++ VayBe/vaybe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG_MODE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 AUTH_USER_MODEL = "users.Users"
 
